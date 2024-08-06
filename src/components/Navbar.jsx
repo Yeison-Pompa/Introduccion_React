3923e5fffde35e01341704d643e08aac448edf2c
Yeison-Pompa <yeisonpompafer@gmail.com>
2024-08-06 18:28:38 -0500

    add variable true po false

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,47 @@
 import React from 'react'
 
+
 const Navbar = () => {
+  const total = 25000;
+  const token = true;
+
+
+
   return (
     <div>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+        <nav  id='bgNav' className="navbar navbar-expand-lg bg-dark-subtle text-emphasis-dark">
+  <div className="container">
+    <a className="navbar-brand" href="#">Pizzeria</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav ms-auto">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
+
+{
+  token ? (
+    <>  <li className="nav-item">
+    <a className="nav-link" href="#">Profile</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link" href="#">Logout</a>
+  </li> </>
+  ) :(
+    <>      <li className="nav-item">
+    <a className="nav-link" href="#">Login</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link" href="#">Register</a>
+  </li></>
+  )
+}
+
+  
+      
+        
       </ul>
     </div>
   </div>
