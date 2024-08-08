db2bbee4a1f4c59f637a90a474d49737ab2127c1
Yeison-Pompa <yeisonpompafer@gmail.com>
2024-08-07 21:00:52 -0500

    add estilos

--- src/components/CardPizza.jsx
+++ src/components/CardPizza.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const CardPizza = () => {
-  return (
-    <div>
+  return <div></div>;
+};
 
-    </div>
-  )
-}
-
-export default CardPizza
+export default CardPizza;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,44 @@
 import React from 'react'
 
 const Header = () => {
+  const estilosContenedor = {
+    position: "relative",
+    width: "100%",
+   
+    overflow: "hidden",
+  };
+
+  const estilosImagen = {
+    width: "100%",
+    
+    objectFit: "cover",
+    display: "block",
+  };
+
+  const estilosTexto = {
+   
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    color: "white", // Puedes ajustar el color según sea necesario
+    textAlign: "center",
+  };
+
   return (
-    <div>
-  
-
-
+    <div style={estilosContenedor}>
+      <img
+        src="http://www.downtownmakeover.com/images/news/pizza-lg.jpg"
+        alt="Imagen de pizza"
+        style={estilosImagen}
+      />
+      <div style={estilosTexto}>
+        <h1 style={{ fontFamily: "fantasy",  fontSize: "40px"}}>¡Pizzería Mamma Mia!</h1>
+        <p>¡Tenemos las mejores pizzas que podras encontrar!</p>
+      </div>
     </div>
-  )
-}
+  );
+};
+
 
 export default Header
