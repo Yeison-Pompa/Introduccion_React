ea9c671e49b01e33cc87d31f0a089a876ac9133f
Yeison-Pompa <yeisonpompafer@gmail.com>
2024-09-23 21:56:42 -0500

    logout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useContext } from "react";
 import { CartContext } from "../context/CartContext";
-import { contador } from "../utils/contador"; // direccionando ruta utils
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // dev de fontawesome
 import { faHouse, faCartShopping } from "@fortawesome/free-solid-svg-icons"; // dev de fontawesome Solid
 import { NavLink } from "react-router-dom"; // Asegúrate de que la importación de Link es correcta
@@ -9,16 +8,17 @@ import { UserContext } from "../context/UserContext";
 
 const Navbar = () => {
   const { total } = useContext(CartContext);
-  /* retornando hacia la vista  */
   const { logout, token } = useContext(UserContext);
+
   return (
     <>
       {/* creando el Navbar con bootstrap */}
       <nav className="navbar navbar-expand-lg navbar-dark bg-black text-white">
         <div className="container">
-          <a className="navbar-brand" href="#">
+          {/* Usamos NavLink en lugar de href="#" */}
+          <NavLink to="/" className="navbar-brand">
             Pizzería Mamma Mia
-          </a>
+          </NavLink>
           <button
             className="navbar-toggler"
             type="button"
@@ -34,52 +34,49 @@ const Navbar = () => {
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <NavLink to="/" className="nav-link active" aria-current="page">
-                  <i className="bi bi-house-door"></i>{" "}
-                  <FontAwesomeIcon icon={faHouse} />
-                  Home
+                  <FontAwesomeIcon icon={faHouse} /> Home
                 </NavLink>
               </li>
-              {/* Operador ternario expresión 1 True */}
+
+              {/* Operador ternario para mostrar rutas dependiendo del estado del token */}
               {token ? (
                 <>
                   <li className="nav-item">
                     <NavLink to="/profile" className="nav-link active">
-                      <i className="bi bi-person"></i> Profile
+                      Profile
                     </NavLink>
                   </li>
                   <li className="nav-item">
-                    <NavLink to="/logout" className="nav-link active">
-                      {/*  <i className="bi bi-box-arrow-right"></i> Logout */}
-                      <button onClick={logout}>Logout</button>
-                    </NavLink>
+                 
+                    {/* <button className="btn btn-link nav-link active" onClick={logout}>
+                      Logout
+                    </button> */}
                   </li>
                 </>
               ) : (
                 <>
-                  {/* Operador ternario expresión 2 False */}
                   <li className="nav-item">
                     <NavLink to="/login" className="nav-link active">
-                      <i className="bi bi-box-arrow-in-right"></i> Login
+                      Login
                     </NavLink>
                   </li>
                   <li className="nav-item">
                     <NavLink to="/register" className="nav-link active">
-                      <i className="bi bi-person-plus"></i> Register
+                      Register
                     </NavLink>
                   </li>
+                  <li className="nav-item m-2">
+                  <button onClick={logout}>Logout</button>
+                </li>
                 </>
               )}
             </ul>
 
-            {/* a Funcion toLocaleString */}
+            {/* Total del carrito */}
             <span className="navbar-text ms-auto">
               <NavLink to="/cart">
                 <button className="btn btn-secondary">
-                  {/* agregando iconos con font awesome */}
-                  <i className="bi bi-cash">
-                    <FontAwesomeIcon icon={faCartShopping} />{" "}
-                  </i>
-                  Total: ${total.toFixed(2)}
+                  <FontAwesomeIcon icon={faCartShopping} /> Total: ${total.toFixed(2)}
                 </button>
               </NavLink>
             </span>
